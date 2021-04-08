a209b0f1bd5b0481e15b942605fa8ce45e4063ac
Kavipriya <kavipriyakavi8@gmail.com>
2021-04-08 12:42:25 +0530

    Maven

---
+++ src/test/java/org/TestNGOne.java
@@ -0,0 +1,35 @@
+package org;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestNGOne {
+	
+	@BeforeClass
+	public void tc() {
+		System.out.println(123);
+	}
+	@AfterClass
+	public void tc1() {
+		System.out.println(12345);
+	}
+	@BeforeMethod
+	public void tc2() {
+		System.out.println(123123);
+	}
+	@AfterMethod
+	public void tc3() {
+		System.out.println("1234567asdfg");
+	}
+	@Test
+	public void tc4() {
+		System.out.println("asdf");
+	}
+	@Test
+	public void tc5() {
+		System.out.println("tc5");
+	}
+}
